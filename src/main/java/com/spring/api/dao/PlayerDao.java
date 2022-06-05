b61cc5be1fb051ae15a05fb8e9c48ba38a1a2bed
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-06-05 05:54:56 -0700

    Declaring Interface

--- src/main/java/com/spring/api/dao/PlayerDao.java
+++ src/main/java/com/spring/api/dao/PlayerDao.java
@@ -4,8 +4,16 @@ import com.spring.api.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
-//@Repository
-@RepositoryRestResource(path="persons")
+@Repository
+//@RepositoryRestResource(path="persons")
 public interface PlayerDao extends JpaRepository<Player, Integer> {
+
+    // http://localhost:8080/api/persons/search/findByName?name=Messi
+    Player findByName(@RequestParam String name);
+
+
+    // http://localhost:8080/api/persons/search/findByNameContaining?name=K
+    Player findByNameContaining(@RequestParam String name);
 }
