bad0ee5102199c4a908d7af21396b0fdebaa3954
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-06-05 05:38:08 -0700

    @RepositoryRestResource

--- src/main/java/com/spring/api/dao/PlayerDao.java
+++ src/main/java/com/spring/api/dao/PlayerDao.java
@@ -2,8 +2,10 @@ package com.spring.api.dao;
 
 import com.spring.api.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
+@RepositoryRestResource(path="persons")
 public interface PlayerDao extends JpaRepository<Player, Integer> {
 }
